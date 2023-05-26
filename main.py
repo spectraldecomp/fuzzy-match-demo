406c7cf5ec96007c1b4daa549015b3f0f6b05beb
spectraldecomp <ecb8918@utulsa.edu>
2023-05-26 10:03:14 -0500

    Admittedly incomplete and a few bugs, but still more concepts

--- main.py
+++ main.py
@@ -1,97 +1,70 @@
-from rapidfuzz import process
+from rapidfuzz import process, fuzz
 
 
-# Dummy list of fake company name strings in 3
-# distinct groups but with slight
-# variations in spelling and punctuation
-strings = [
-    "Acme, Inc.",
-    "Acme Inc",
-    "Acme, Inc",
-    "Acme Inc.",
-    "Acme, Incorporated",
-    "Acme Incorporated",
-    "Acme, Incorporated.",
-    "Acme Incorporated.",
-    "Acme, Inc. & Co.",
-    "Acme Inc & Co",
-    "Acme, Inc & Co",
-    "Acme Inc. & Co",
-    "Acme, Incorporated & Co.",
-    "Acme Incorporated & Co.",
-    "Acme, Incorporated. & Co.",
-    "Acme Incorporated. & Co.",
-    "Acme, Inc. and Co.",
-    "Acme Inc and Co",
-    "Acme",
-    "Acme, LLC",
-    "AMCE",
-    "ACMOSON",
-    "fghn",
-    "Fellow God Hello Now",
-    "Fellow God Hello Now, Inc.",
-    "Fellow God Hello Now Inc",
-    "Fellow God Hello Now, Inc",
-    "Fellow God Now Inc.",
-    "FGHN INC",
-    "Granted Now Inc",
-    "Granted Now, Inc.",
-    "Granted Now Inc.",
-    "GNI INC",
-    "GNI, Inc.",
-    "GNI Inc.",
-    "Granted Now Incorporated"
-    ]
+# Loads company data with user-specified keywords for each company
+def load_companies(file_path):
+    companies = {}
 
-# We now wish to sort the list of strings into 3 groups based
-# off of several provided keywords. We will use process.extract.
+    with open(file_path, 'r') as file:
+        num_companies = int(file.readline().strip())
 
-# First, we will create a list of keywords to search for
+        for _ in range(num_companies):
+            line = file.readline().strip()
+            company_name, company_keywords = line.split(':')
+            company_name = company_name.strip()
+            company_keywords = [keyword.strip() for keyword in company_keywords.strip('[]').split(',')]
 
-keywords = ["Acme", "Granted Now", "Fellow God Hello Now"]
+            companies[company_name] = company_keywords
 
-# Next, we will create a list of lists to store the results
+    return companies
 
-results = [[], [], []]
+# Loads data to be processed
+def load_data(file_path):
+    data = []
 
-# Now, we will iterate through the list of keywords and use
-# process.extract to find the best match for each keyword
+    with open(file_path, 'r') as file:
+        num_data = int(file.readline().strip())
 
-# process.extract returns a list of tuples with 3 elements:
-# 1. The best match for the keyword
-# 2. The score of the best match
-# 3. The index of the best match in the original list
+        for _ in range(num_data):
+            line = file.readline().strip()
+            data.append(line)
 
-for i in range(len(keywords)):
-    results[i] = process.extract(keywords[i], strings, limit=len(strings))
+    return data
 
-#We will print the results
+ # Finds relevance of each data point to specified keyword and its associated company
+def process_keyword(keyword, data):
 
-for i in range(len(keywords)):
-    print("Keyword: " + keywords[i])
-    print("Results: " + str(results[i]))
-    print("")
+    print(f'Processing keyword: {keyword}')
+    x = process.extract(keyword, data, limit=len(data))
+    return x
 
-# Clearly, the results are not perfect. We can see that there are slight errors on the borders. Still,
-# only 1 or 2 companies are misclassified. We now have three lists with the correct matches for each keyword
-# mostly at the front of the list. Let's make the lists a bit less verbose.
+def main():
+    companies = load_companies('data/companies.txt')
+    data = load_data('data/data.txt')
+    sorted_data = []
+    for company in companies:
+        sorted_company_data = []
+        for keyword in companies[company]:
+            sorted_keyword = process_keyword(keyword, data)
+            # Add each element of sorted_keyword to sorted_company_data if it is not already in sorted_company_data.
+            # We check if it is already in sorted_company_data by checking if the third element of the tuple is present in
+            # any of the elements of sorted_company_data. If it is not, then we add that element. #We also overwrite the element
+            # if it is already present in sorted_company_data but with a lower score.
+            for element in sorted_keyword:
+                if not any(element[2] in sublist for sublist in sorted_company_data):
+                    sorted_company_data.append(element)
+                else:
+                    for i in range(len(sorted_company_data)):
+                        if sorted_company_data[i][2] == element[2] and sorted_company_data[i][1] < element[1]:
+                            sorted_company_data[i] = element
+        sorted_company_data.sort(key=lambda x: x[1], reverse=True)
+        sorted_data.append(sorted_company_data)
 
-# We will create a new list to store the results
+    print((sorted_data[1]))
 
-results2 = [[], [], []]
 
-# We will iterate through the results and append the first element of each tuple to the new list
 
-for i in range(len(keywords)):
-    for j in range(len(results[i])):
-        results2[i].append(results[i][j][0])
 
-# We will print the results
-
-for i in range(len(keywords)):
-    print("Keyword: " + keywords[i])
-    print("Results: " + str(results2[i]))
-    print("")
-
-# It is up to the user to determine the cutoff points for each list.
+if __name__ == '__main__':
+    main()
 
